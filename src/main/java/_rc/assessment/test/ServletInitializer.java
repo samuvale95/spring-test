eeb3021347829c16eaa72f3eabbc1830027590e0
2RC_GT <giuseppe.trieste@2rcapital.com>
2024-12-19 14:45:46 +0100

    Spring initializr

---
+++ src/main/java/_rc/assessment/test/ServletInitializer.java
@@ -0,0 +1,13 @@
+package _rc.assessment.test;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+}
